41c5afb48d32975ff9ac7efd3a75c4a8f2523d01
VladFiliucov <filiucov@mail.ru>
2016-01-13 10:55:22 +0000

    Apply :let to car project.

--- spec/lib/car_project/car_spec.rb
+++ spec/lib/car_project/car_spec.rb
@@ -4,49 +4,51 @@ describe 'Car' do
   
   describe 'attributes' do
 
-    before(:example) do
-      @car = Car.new
-    end
+    let(:car) { @car = Car.new }
+    
     it "allows reading and writing for :make" do
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+      car.year = 9999
+      expect(car.year).to eq(9999)
     end
 
     it "allows reading and writing for :color" do
-      @car.color = 'foo'
-      expect(@car.color).to eq('foo')
+      car.color = 'foo'
+      expect(car.color).to eq('foo')
     end
 
     it "allows reading and writing for :wheels" do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
 
     it "allows writing for :doors"
   end
 
   describe "colors" do
+
+    let(:colors) { ['blue', 'black', 'purple', 'red']}
+
     it "returns an array of color names" do
-      c = ['blue', 'black', 'purple', 'red']
-      expect(Car.colors).to match_array(c)
+      expect(Car.colors).to match_array(colors)
     end
   end
 
   describe "#full_name" do
 
+    let(:honda) { Car.new(make: 'Honda', year: 2004, color: 'blue')}
+    let(:new_car) {Car.new}
+
     it "returns a string in the expected format" do
-      @honda = Car.new(make: 'Honda', year: 2004, color: 'blue')
-      expect(@honda.full_name).to eq('2004 Honda (blue)')
+      expect(honda.full_name).to eq('2004 Honda (blue)')
     end
 
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        car = Car.new
-        expect(car.full_name).to eq('2007 Volvo (unknown)')
+        expect(new_car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
   end
